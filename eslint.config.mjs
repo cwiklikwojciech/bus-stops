b2d28317be6b148724ad2040a9be3fbc98192887
Wojtek <cwiklik.wojciech@gmail.com>
2026-05-07 11:38:12 +0200

    chore: update all package

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  {
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+  }
+)
